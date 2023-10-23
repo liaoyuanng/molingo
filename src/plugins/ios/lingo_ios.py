2e2efb676542be6a6b25dbb5568878ffee6b65ce
Liaoyuan Ju <liaoyuanng@gmail.com>
2023-10-23 09:48:04 +0800

    feat: add post load script

--- src/plugins/ios/lingo_ios.py
+++ src/plugins/ios/lingo_ios.py
@@ -8,6 +8,7 @@ import re
 class LingoIOS(ILingoPlugin):
     
     __platform: Platform
+    __main_language = ""
 
     def __update_strings_file(self, df, language, filename, dir):
         if not utils.check_is_dir(dir):
@@ -20,7 +21,7 @@ class LingoIOS(ILingoPlugin):
         with open(file_path, write_mode) as file:
             if write_mode == 'w' or utils.is_empty_file(file_path):
                 file.write(utils.do_not_edit_tips())
-            for index, row in df.iterrows():
+            for row in df.iterrows():
                 if not re.search(r'(?<!\\)"', row[language]):
                     value = row[language]
                 else:
@@ -39,11 +40,24 @@ class LingoIOS(ILingoPlugin):
         return lproj_path
     
     def post_load(self):
-        pass
+        autogen_dir = os.path.join(self.__platform.output, "molingo")
+        if not utils.check_is_dir(autogen_dir):
+            os.makedirs(autogen_dir, exist_ok=True)
+            utils.run['swiftgen', 'config', 'init']
+            with open(os.path.join(autogen_dir, 'swiftgen.yml'), 'a') as file:
+                file.write(f"""
+                           strings:
+                             inputs: {self.__main_language}
+                             outputs:
+                               templateName: structured-swift5
+                               output: ../Resources/Generated/Strings.swift
+                           """)
+        utils.run["swiftgen"]
+        
         
     def pre_load(self):
         swiftgen = "swiftgen"
-        if utils.tool_path(swiftgen) == None:
+        if utils.get_command_path(swiftgen) == None:
             utils.install_with_brew(swiftgen)
 
     def load(self, csv_df, platform: Platform):
@@ -51,8 +65,10 @@ class LingoIOS(ILingoPlugin):
         self.__platform = platform
         # get languages from csv file
         languages = csv_df.columns[1:]
-        for language in languages:
+        for index, language in languages:
             dir = self.__create_lproj_dir(language)
+            if index == 0:
+                self.__main_language = dir
             filename = f'{language}.strings'
             self.__update_strings_file(csv_df, language, filename, dir)
         
