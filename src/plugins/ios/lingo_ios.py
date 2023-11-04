8f125322bb0d15e9b362162ae563e707d935e5d9
liaoyuanng <liaoyuanng@gmail.com>
2023-11-04 14:37:34 +0800

    feat: replace value when same key

--- src/plugins/ios/lingo_ios.py
+++ src/plugins/ios/lingo_ios.py
@@ -21,16 +21,41 @@ class LingoIOS(ILingoPlugin):
         
         file_path = os.path.join(dir, filename)
         
-        with open(file_path, write_mode) as file:
-            if write_mode == 'w' or utils.is_empty_file(file_path):
-                file.write(utils.do_not_edit_tips())
+        lines = []
+        existing_keys = {}
+        # read exist file content
+        if write_mode == 'a' and not utils.is_empty_file(file_path):
+            with open(file_path, 'r') as file:
+                lines = file.readlines()
+                # create a dictionary of existing keys and their positions in the list of lines
+                for i, line in enumerate(lines):
+                    match = re.match(r'"(.*)"\s=\s".*";', line)
+                    if match:
+                        key = match.group(1)
+                        existing_keys[key] = i
+                        
+        # open file with write mode
+        with open(file_path, "w") as file:
             for _, row in df.iterrows():
+                # remove escape character
                 if not re.search(r'(?<!\\)"', row[language]):
                     value = row[language]
                 else:
                     value = row[language].replace('"', '\\"')
                 value = value.replace('\n', '\\n')
-                file.write(f'"{row["key"]}" = "{value}";\n')
+                new_line = f'"{row["key"]}" = "{value}";\n'
+                # if write mode is 'a' => replace
+                # if write mode is 'w' => cover
+                if write_mode == 'a':
+                    if row["key"] in existing_keys:
+                        lines[existing_keys[row["key"]]] = new_line
+                    else: 
+                        lines.append(new_line)
+                        
+            if (not utils.do_not_edit_tips() in lines):            
+                lines.insert(0, utils.do_not_edit_tips())
+            for line in lines:
+                file.write(line)
                 
     def __create_lproj_dir(self, language):
         files = os.listdir(self.__platform.proj_root_path)
@@ -98,8 +123,8 @@ class LingoIOS(ILingoPlugin):
             if index == 0:
                 self.__main_language = language
             filename = f'Localizable.strings'
-            utils.log(f"[{language}] localization done.")
             self.__update_strings_file(csv_df, language, filename, dir)
+            utils.log(f"[{language}] localization done.")
         self.post_load()
         
 
